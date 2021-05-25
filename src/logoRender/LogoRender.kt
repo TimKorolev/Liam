77b4b6d873ba06e32523be90fa329bd914b824b0
Timofei Korolev <timofei.korolev@jetbrains.com>
2021-05-25 19:22:03 +0300

    ss

--- src/logoRender/LogoRender.kt
+++ src/logoRender/LogoRender.kt
@@ -32,7 +32,7 @@ class LogoRender {
                     fill="url(#Gradient_${key})"/>
                 </g>
                 """
-                defs += """<linearGradient id="Gradient222234_${key}">
+                defs += """<linearGradient id="Gradient2222345_${key}">
                     <stop offset="0" stop-color="${ray.startPoint.color.hex}"></stop>
                     <stop offset="1" stop-color="${ray.endPoint.color.hex}"></stop>
                     </linearGradient>"""
